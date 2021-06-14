912f1726349c4f7a1ab3b14b5f7a91c7f5220ecf
segaman <sergey02047747@gmail.com>
2021-06-14 21:04:18 +0300

    add fragments

--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -2,10 +2,34 @@ package com.rsschool.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rsschool.quizn.FirstFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),FirstFragment.OpenNextQuestion,SecondFragment.OpenPrevQuestion {
+    private val quiz=FirstFragment()
+    private val quizTwo=SecondFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val transaction= supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainerView,quiz)
+        transaction.commit()
     }
+
+    override fun OpenSecondQuestion() {
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.fragmentContainerView,quizTwo)
+                commit()
+            }
+    }
+
+    override fun OpenFirstQuestion() {
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.fragmentContainerView,quiz)
+                commit()
+            }
+    }
+
+
 }
